7f9d359e6282ac8d86f238dd4d209ad06fca2c21
Pippin <pippin.barr@gmail.com>
2021-01-15 10:34:19 -0500

    Added hopefully exemplary Slamina code

---
+++ activities/slamina/js/script.js
@@ -0,0 +1,266 @@
+"use strict";
+
+/*****************
+
+Slamina
+Pippin Barr
+
+A guessing game in which the page pronounces the name of an animal
+backwards and the user has to figure out what it was and say the
+name forwards in order to get the next animal.
+
+******************/
+
+// An array of animal names from
+// https://github.com/dariusk/corpora/blob/master/data/animals/common.json
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
+];
+
+const QUESTION_DELAY = 2000; // in milliseconds
+
+// The current answer to display (we use it initially to display the click instruction)
+let currentAnswer = `Click to begin.`;
+// The current animal name the user is trying to guess
+let currentAnimal = ``;
+
+
+/**
+Create a canvas
+Set up annyang with the guessing command
+Set text defaults
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Is annyang available?
+  if (annyang) {
+    // Create the guessing command
+    let commands = {
+      'I think it is *animal': guessAnimal
+    };
+    // Setup annyang and start
+    annyang.addCommands(commands);
+    annyang.start();
+  }
+
+  // Text defaults
+  textSize(102);
+  textStyle(BOLD);
+  textAlign(CENTER);
+}
+
+
+/**
+Display the current answer in red if incorrect and green if correct
+(Display nothing if no guess entered yet)
+*/
+function draw() {
+  background(0);
+
+  if (currentAnswer === currentAnimal) {
+    fill(0, 255, 0);
+  }
+  else {
+    fill(255, 0, 0);
+  }
+  text(currentAnswer, width / 2, height / 2);
+}
+
+/**
+Reverse the current animal name and say it with ResponsiveVoice
+*/
+function sayCurrentAnimal() {
+  let reverseAnimal = reverseString(currentAnimal);
+  responsiveVoice.speak(reverseAnimal);
+}
+
+/**
+Reverses the provided string
+*/
+function reverseString(string) {
+  // Split the string into an array of characters
+  let characters = string.split('');
+  // Reverse the array of characters
+  let reverseCharacters = characters.reverse();
+  // Join the array of characters back into a string
+  let result = reverseCharacters.join('');
+  // Return the result
+  return result;
+}
+
+/**
+Called by annyang when the user make a guess.
+animal parameter contains the guess as a string.
+Checks if the guess is correct or not.
+*/
+function guessAnimal(animal) {
+  // Conver the guess to lowercase to match the answer format
+  currentAnswer = animal.toLowerCase();
+  // Check if the answer is correct...
+  if (currentAnswer === currentAnimal) {
+    // If it is correct, go to the next question after a delay
+    setTimeout(nextQuestion, QUESTION_DELAY);
+  }
+  else {
+    // If it isn't correct, process the wrong answer
+    setTimeout(wrongAnswer, QUESTION_DELAY);
+  }
+}
+
+/**
+Reset the answer text on the canvas, and say the animal again
+*/
+function wrongAnswer() {
+  currentAnswer = ``;
+  sayCurrentAnimal();
+}
+
+/**
+Reset the answer text, get a new random animal, say its name
+*/
+function nextQuestion() {
+  currentAnswer = ``;
+  currentAnimal = random(animals);
+  sayCurrentAnimal();
+}
+
+/**
+When the user clicks, go to the next question
+*/
+function mousePressed() {
+  nextQuestion();
+}
